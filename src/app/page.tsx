281221869de3f540eadb8e7a0e3b39ac5dc69d0e
Edmilson Soares <esbnet@gmail.com>
2022-11-16 18:21:23 -0300

    refactor: :recycle: layout ajust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
-
 export default function Page() {
-  return <h1 className="bg-orange-700 w-auto">Home</h1>
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-6xl mt-28'>Olá,</h1>
+      <h2 className='text-2xl mt-10'>Seja bem vindo!</h2>
+      <span className='mt-10'>
+        Projeto e codifico sites, aplicações web e aplicativos para celulares.
+      </span>
+      <div className='flex flex-col'>
+          <form action='' className='flex flex-col'>
+            <label htmlFor=''>Email</label>
+            <input className='h-8 mb-2 rounded p-2' type='email' required/>
+            <label htmlFor=''>Mensagem</label>
+            <textarea
+              className="rounded p-2"
+              name=''
+              id=''
+              cols={40}
+              rows={10}
+              placeholder='Deixe seu questionamento'
+            ></textarea>
+            <button className="w-full h-8 bg-stone-900 rounded my-2">Enviar</button>
+          </form>
+      </div>
+    </div>
+  )
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { AiOutlineCopyrightCircle } from "react-icons/Ai"
 
 export default function Footer() {
   return (
-    <div className='flex items-center justify-between bg-slate-300 fixed bottom-0 w-[1280px]  px-2 h-8 text-sm text-slate-600'>
+    <div className='flex items-center justify-between bg-slate-300 fixed bottom-0 w-[1100px]  px-2 h-8 text-sm text-slate-600'>
       <p className="flex">
         powered by:<strong className="flex mx-2">quarks_Code<AiOutlineCopyrightCircle className="text-[10px]"/>
         </strong>
